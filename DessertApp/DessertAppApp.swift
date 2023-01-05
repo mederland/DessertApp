4a98a17258adae9950e347e03e7141fa91eb7018
mederland <mederland@gmail.com>
2023-01-05 15:54:18 -0600

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Meder iZimov on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
